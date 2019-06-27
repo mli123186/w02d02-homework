a1931d81fd5a949ce32e2752aaa0ab3b611270bb
Dongyuan Li <dongyuanli@Dongyuans-MacBook-Pro.local>
2019-06-26 18:23:24 -0600

    Pokemon not done

---
+++ js/app.js
@@ -0,0 +1,100 @@
+console.log("Pokemon-HW")
+
+const cards = [
+  {
+    name: "Bulbasaur",
+    damage: 60
+  }, {
+    name: "Caterpie",
+    damage: 40
+  }, {
+    name: "Charmander",
+    damage: 60
+  }, {
+    name: "Clefairy",
+    damage: 50
+  }, {
+    name: "Jigglypuff",
+    damage: 60
+  }, {
+    name: "Mankey",
+    damage: 30
+  }, {
+    name: "Meowth",
+    damage: 60
+  }, {
+    name: "Nidoran - female",
+    damage: 60
+  }, {
+    name: "Nidoran - male",
+    damage: 50
+  }, {
+    name: "Oddish",
+    damage: 40
+  }, {
+    name: "Pidgey",
+    damage: 50
+  }, {
+    name: "Pikachu",
+    damage: 50
+  }, {
+    name: "Poliwag",
+    damage: 50
+  }, {
+    name: "Psyduck",
+    damage: 60
+  }, {
+    name: "Rattata",
+    damage: 30
+  }, {
+    name: "Squirtle",
+    damage: 60
+  }, {
+    name: "Vulpix",
+    damage: 50
+  }, {
+    name: "Weedle", 
+    damage: 40
+  }
+];
+
+const player = {
+	name: "Eggbert",
+	
+	playerPickcards(cards) {
+		if(cards.length >= 3) {
+			pickcards();
+		}
+	}
+
+}
+
+const computer = {
+	name: "Computer",
+	
+	computerPickcards(cards) {
+		if(cards.length>=3) {
+			pickcards();
+		}
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
