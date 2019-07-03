5b3fa062a0f8408949e3c07551a8a965f59ac6be
Dongyuan Li <dongyuanli@Dongyuans-MacBook-Pro.local>
2019-07-03 10:23:53 -0600

    Done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 console.log("Pokemon-HW")
 
-const cards = [
+const game = {
+cards: [
   {
     name: "Bulbasaur",
     damage: 60
@@ -56,29 +57,113 @@ const cards = [
     name: "Weedle", 
     damage: 40
   }
-];
+],
+
+round: 0
+
+};
 
 const player = {
-	name: "Eggbert",
-	
-	playerPickcards(cards) {
-		if(cards.length >= 3) {
-			pickcards();
-		}
-	}
-
-}
+	//name: "Eggbert",
+	name: "player",
+	hands: [],
+	score: 0,
+	winRound: 0,
+};
 
 const computer = {
-	name: "Computer",
-	
-	computerPickcards(cards) {
-		if(cards.length>=3) {
-			pickcards();
-		}
+	name: "computer",
+	hands: [],
+	score: 0,
+	winRound: 0,
+};
 
+function removeCards() {
+	return game.cards.splice(Math.floor(Math.random()*game.cards.length), 1)[0];
+}
+
+function dealCards() {
+	player.hands = [];
+	computer.hands = [];
+	if(game.cards.length >= 6) {
+		for (let i = 0; i < 3; i++){
+			player.hands.push(removeCards());
+			computer.hands.push(removeCards());
+
+		}
+	}
+	 //console.log(`the player's cards: ${JSON.stringify(player.hands)}, the computer's cards: ${JSON.stringify(computer.hands)}`);
+		showCards();
+	}
+//dealCards();
+	//console.log(dealCards());
+
+function showCards() {
+	//dealCards();
+	for (let i = 0; i < player.hands.length; i++) {
+
+		console.log(`player: ${JSON.stringify(player.hands[i].name)} ${JSON.stringify(player.hands[i].damage)} 
+					computer: ${JSON.stringify(computer.hands[i].name)} ${JSON.stringify(computer.hands[i].damage)}`)
 	}
 }
+
+function cardBattle() {
+	dealCards();
+	player.score = 0;
+	computer.score = 0;
+	for (let i = 0; i< player.hands.length; i++) {
+		if (player.hands[i].damage > computer.hands[i].damage) {
+			player.score += 1;
+			console.log(`player score: ${player.score}`);
+		} else if (player.hands[i].damage < computer.hands[i].damage) {
+			computer.score += 1;
+			console.log(`computer score: ${computer.score}`);
+		} else {
+			console.log("Tie Game!");
+		}
+	}
+}
+
+function winRound() {
+	cardBattle();
+
+	if(player.score > computer.score) {
+		player.winRound += 1;
+		game.round += 1;
+		console.log(`player win round ${game.round}`)
+	} else if(player.score < computer.score) {
+		computer.winRound +=1;
+		game.round += 1;
+		console.log(`computer win round ${game.round}`)
+	} else	{
+		console.log("Tie Round!");
+		game.round += 1;
+	}
+}
+
+function battleRound() {
+	while(game.cards.length >= 6) {
+		//dealCards();
+		//showCards();
+		//cardBattle();
+		winRound();
+	}
+}
+
+winRound();
+function winner() {
+	if (player.winRound > computer.winRound) {
+		console.log("Winner is player");
+	} else if (player.winRound < computer.winRound) {
+		console.log("Winner is computer");
+	} else {
+		console.log("Tie Game");
+	}
+}
+
+battleRound();
+winner();
+// result.push(sourceArray[Math.floor(Math.random()*sourceArray.length)]);
 
 
 
